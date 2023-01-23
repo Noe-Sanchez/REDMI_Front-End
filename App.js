4154aa41f99ebe6d4dc408d5894bb0727e046acd
Noah-Sanchez <danfoot4567@gmail.com>
2023-01-23 06:20:23 +0000

    Working backend primitive requests

--- App.js
+++ App.js
@@ -1,19 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react' 
+import { StyleSheet, Text, View, Button} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Que show que show</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class MainView extends Component{
+  render(){
+    console.log('Rendering MainView')
+    return(
+      <APIviewer/>
+    );
+  }
+}
+
+class APIviewer extends Component{
+  makeFetch(){
+    console.log('Button trying a Backend fetch');
+    fetch('http://localhost:19004/api', {method: "GET"})
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }
+
+  render(){
+    console.log('Rendering API viewer')
+    return(
+      <View style={styles.container}>
+        <Text style={styles.baseText}>Prueba del front</Text>
+        <Button
+	  onPress={this.makeFetch}
+	  title="Hacer fetch al back"
+	  style={styles.baseButton}
+	/>
+      </View>);
+  }
 }
 
 const styles = StyleSheet.create({
+  baseText: {
+    fontFamily: 'Consolas',
+    color: '#FFF',
+  },
+  baseButton: {
+    fontFamily: 'Consolas',
+    color: '#F60',
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#444',
     alignItems: 'center',
     justifyContent: 'center',
   },
